02b51fda50e4d8d9b7ab724f2ab5cad0542e94fe
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-01-11 22:37:33 +0100

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,17 +5,6 @@ import './index.css';
 import { VisibilityProvider } from './providers/VisibilityProvider';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './theme';
-import { isEnvBrowser } from './utils/misc';
-
-if (isEnvBrowser()) {
-  const root = document.getElementById('root');
-
-  // https://i.imgur.com/iPTAdYV.png - Night time img
-  root!.style.backgroundImage = 'url("https://i.imgur.com/3pzRj9n.png")';
-  root!.style.backgroundSize = 'cover';
-  root!.style.backgroundRepeat = 'no-repeat';
-  root!.style.backgroundPosition = 'center';
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
